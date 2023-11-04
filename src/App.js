5082df47d12d80a53a14a14e7c902f978b0f1a0f
whichlight <whichlight@gmail.com>
2023-11-03 22:59:20 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Data from "./data.json";
+import "./App.css";
 
-function App() {
+const TreeNode = ({ node }) => {
+  const [isOpen, setOpen] = useState(false);
+  const [activeNode, setActiveNode] = useState(null);
+
+  const handleToggle = (name) => {
+    setOpen(!isOpen);
+    setActiveNode(name);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <li>
+      <div
+        onClick={() => handleToggle(node.name)}
+        className={`node ${activeNode === node.text ? "active" : ""}`}
+      >
+        {node.text}
+      </div>
+      {isOpen && node.children && (
+        <ul className="child-nodes">
+          {node.children.map((childNode, index) => (
+            <TreeNode node={childNode} key={index} />
+          ))}
+        </ul>
+      )}
+    </li>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="app">
+      <ul>
+        <TreeNode node={Data} />
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
